d7c4e1184bee021ae9f31c89a75a98dd172cbbe0
Long Nguyen <nclong87@gmail.com>
2021-07-26 13:48:02 +0700

    Updates

---
+++ precache-manifest.04bbc4c696592217c47c09eede379635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b7ab020faa0caa5e6432338bfb458f",
+    "url": "/pdfwriter/index.html"
+  },
+  {
+    "revision": "68446e7e1fe16647c135",
+    "url": "/pdfwriter/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "bcad44e745e019a67266",
+    "url": "/pdfwriter/static/js/2.329e12b3.chunk.js"
+  },
+  {
+    "revision": "ae0727c092f1c9916f0a1bb5935705c6",
+    "url": "/pdfwriter/static/js/2.329e12b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68446e7e1fe16647c135",
+    "url": "/pdfwriter/static/js/main.0c6fe972.chunk.js"
+  },
+  {
+    "revision": "ed99650e788f68d63692",
+    "url": "/pdfwriter/static/js/runtime-main.42a109b1.js"
+  }
+]);
